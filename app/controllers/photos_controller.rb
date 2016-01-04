87f737ce1e5002f7b318d49b72ed872eaf40e474
Daniel Adler <weston.adler@gmail.com>
2016-01-04 15:11:15 -0800

    add controllers, views and routes for app, start detailing index view

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,17 @@
+class PhotosController < ApplicationController
+
+  def index
+    @photos = Photo.all
+
+  end
+
+  def show
+  end
+
+  def rate
+  end
+
+  def rankings
+  end
+
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,12 @@
+class UsersController < ApplicationController
+
+  def sign_up
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+end
