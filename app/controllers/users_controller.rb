afcf361d672ff26f0830913e5dc0a71ccb9ef2a2
danieladler <weston.adler@gmail.com>
2016-01-11 15:50:12 -0800

    implement gravatar image and default in navbar, account show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,10 +29,4 @@ class UsersController < ApplicationController
     end
   end
 
-  def avatar_url(user)
-    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
-    "http://gravatar.com/avatar/#{gravatar_id}.png"
-  end
-  helper_method :avatar_url
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,12 @@ class User < ActiveRecord::Base
   has_many :photos
   has_many :ratings
 
+  def avatar
+    default_url = "http://placekitten.com/300/301"
+    gravatar_id = Digest::MD5.hexdigest(self.email.downcase)
+    "http://gravatar.com/avatar/#{gravatar_id}.png?d=#{CGI.escape(default_url)}"
+  end
+
   # add random_photo method
 
 end
