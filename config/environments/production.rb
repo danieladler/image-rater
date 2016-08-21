0f288d09a7d15771f74d8f6a8fcb24257980c1ad
danieladler <weston.adler@gmail.com>
2016-08-20 18:32:49 -0700

    update production domain again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'image-rater-dadler.herokuapp.com',
+    domain:               'smtp.gmail.com',
     user_name:            ENV["MAIL_USERNAME"],
     password:             ENV["MAIL_PASSWORD"],
     authentication:       'plain',
